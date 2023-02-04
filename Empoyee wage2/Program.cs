269917ea0d7d42bcc4009da042810f30a6a871a6
madheshvarma <madheshvarma369@gmail.com>
2023-02-04 07:55:18 +0530

    yes

---
+++ Empoyee wage2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Empoyee_wage2
+{
+    internal class Program
+    {
+        public class EmployeeCheck
+        {
+            public const int partTime = 1;
+            public const int fullTime = 2;
+            public const int PerHour = 20;
+            public static void CheckAttendence()
+            {
+
+                int empHrs = 0;
+                int empWage = 0;
+
+                Random random = new Random();
+                int empCheck = random.Next(2);
+
+                switch (empCheck)
+                {
+                    case partTime:
+                        empHrs = 4;
+                        break;
+                    case fullTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * PerHour;
+                Console.WriteLine("Employee Wage: " + empWage);
+            }
+        }
+    }
+}
+
+
+
